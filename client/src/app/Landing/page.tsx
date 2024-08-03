944755797531cb4f6dff41453897525d08e5f24d
Michael Kleyman <michaelkleyman99@gmail.com>
2024-08-02 21:09:25 -0400

    Added material ui theme

---
+++ client/src/app/Landing/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Style } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+type Props = {};
+
+export const Landing = (props: Props) => {
+  return <Box>Landing Page</Box>;
+};
---
+++ client/src/app/wrapper.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Wrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>;
+};

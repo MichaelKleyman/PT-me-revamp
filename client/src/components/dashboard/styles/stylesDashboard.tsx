3c395f0255d562215dd4b02ecfb25902e0419829
Michael Kleyman <michaelkleyman99@gmail.com>
2024-08-30 21:47:37 -0400

    feat: added some lorem ipsum dashboard mockup design

--- client/src/components/dashboard/styles/stylesDashboard.tsx
+++ client/src/components/dashboard/styles/stylesDashboard.tsx
@@ -1,16 +1,9 @@
-import { Stack, styled } from "@mui/material";
-import Link from "next/link";
+import { Link, Stack, styled } from "@mui/material";
 
 export const StyledStack = styled(Stack)(({ theme }) => ({
-  position: "fixed",
-  top: 0,
-  background: theme.palette.background.default,
-  borderRadius: theme.shape.borderRadius * 4,
-  margin: theme.spacing(4),
-  padding: theme.spacing(2),
-  width: "95%",
-  boxShadow: "0px 4px 12px rgba(0.2, 0, 0, 0.2)",
-  zIndex: 3,
+  marginTop: theme.spacing(15),
+  padding: theme.spacing(10),
+  height: "100%",
 }));
 
 export const StyledButtons = styled(Link)(({ theme }) => ({

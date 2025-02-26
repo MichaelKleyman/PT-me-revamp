100b230fee8f922368796c4accbd4130ebc00155
Michael Kleyman <michaelkleyman99@gmail.com>
2025-02-25 20:18:49 -0500

    successfully created users table and connected drizzle to my postgres rds

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,11 +1,17 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+/** Development environment configuration file */
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    host: process.env.DATABASE_HOST!,
+    port: parseInt(process.env.DATABASE_PORT!),
+    user: process.env.DATABASE_USER!,
+    password: process.env.DATABASE_PASSWORD!,
+    database: process.env.DATABASE_NAME!,
+    ssl: { rejectUnauthorized: false },
   },
 });

bc19e7e97246f6c98abe6db852eda2082c720f4d
MichaelKleyman <michaelkleyman99@gmail.com>
2025-10-08 22:33:07 -0400

    set up mock data to visualize the patient exercise data being used on the client

--- client/src/lib/types/exercise.ts
+++ client/src/lib/types/exercise.ts
@@ -4,28 +4,13 @@ export enum ExerciseDifficulty {
   ADVANCED = "advanced",
 }
 
-export enum ExerciseStatus {
-  ACTIVE = "active",
-  COMPLETED = "completed",
-  PENDING = "pending",
-  SKIPPED = "skipped",
-}
-
 export type Exercise = {
   id: string;
   name: string;
   description: string;
-  sets: number;
-  reps: string;
   duration: string;
   benefits: string[];
   equipment: string;
-  frequency: string;
-  status:
-    | ExerciseStatus.ACTIVE
-    | ExerciseStatus.COMPLETED
-    | ExerciseStatus.PENDING
-    | ExerciseStatus.SKIPPED; // TODO: consider removing
   difficulty: ExerciseDifficulty;
   targetArea: string;
   instructions: string[];
